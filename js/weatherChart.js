7ada43146b4c9792e98126650549fdec60961e5b
github username <github email address>
2017-05-02 12:00:12 -0400

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,40 @@
 function getFahrenheits(result){
-  // Your code goes here
+  return result.hourly_forecast.map(function(f){
+    // console.log(f.temp.english)
+    return f.temp.english
+  })
 }
 
 function getHours(result){
-  // Your code goes here
+  return result.hourly_forecast.map(function(f){
+    // console.log(f.FCTTIME.hour)
+    return f.FCTTIME.hour
+  })
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var obj = {
+	labels: labels,
+	datasets: [
+		{
+			fillColor: "rgba(220,220,220,0.2)",
+			strokeColor: "rgba(220,220,220,1)",
+			pointColor: "rgba(220,220,220,1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "rgba(220,220,220,1)",
+			data: data
+		},
+	]}
+;
+return obj
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    method: 'get',
+    url: endpoint,
+    dataType: 'json',
+    success: success
+  })
 }
